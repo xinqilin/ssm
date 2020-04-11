2428e18ed1e58fd26f6e0a410da7e51079608009
BillLin1209 <zzx123bill@gmail.com>
2020-04-11 23:09:44 +0800

    json pom.xml  version down to 2.10.0

--- src/main/java/com/ssm/controller/EmpController.java
+++ src/main/java/com/ssm/controller/EmpController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.ssm.bean.Employee;
@@ -20,31 +21,43 @@ import com.ssm.service.EmpService;
 @Controller
 public class EmpController {
 
-	
 	@Autowired
 	EmpService empService;
 
-//	@GetMapping("/emp{id}")
-//	public Employee getOneEmp(@PathVariable("id") Integer id) {
-//		Employee e=empService.getOne(id);
-//		return empService.getOne(id);
-//	}
-	
-	@GetMapping("/getAll")
-	public String getAll(@RequestParam(value="pn",defaultValue="1") Integer pn,Model model) {
-		
-		//每頁
+
+
+//	ajax寫法
+	@ResponseBody
+	@GetMapping("/getAllReturnJson")
+	public PageInfo getAllReturnJson(@RequestParam(value = "pn", defaultValue = "1") Integer pn) {
+
+		// 每頁
 		PageHelper.startPage(pn, 5);
-		List<Employee> list=empService.getAll();
-		
-		//下標顯示多少頁
-		PageInfo<Employee> pageInfo=new PageInfo<Employee>(list,3);
-		model.addAttribute("pageInfo",pageInfo);
+		List<Employee> list = empService.getAll();
+
+		// 下標顯示多少頁
+		PageInfo<Employee> pageInfo = new PageInfo<Employee>(list, 3);
+//		model.addAttribute("pageInfo", pageInfo);
+		return pageInfo;
+	}
+	
+	
+
+//	頁面跳轉
+//	@GetMapping("/getAll")
+	public String getAll(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) {
+
+		// 每頁
+		PageHelper.startPage(pn, 5);
+		List<Employee> list = empService.getAll();
+
+		// 下標顯示多少頁
+		PageInfo<Employee> pageInfo = new PageInfo<Employee>(list, 3);
+		model.addAttribute("pageInfo", pageInfo);
 //		System.out.println("size:"+list.size()+",   list is empty?"+list.isEmpty());
 //		System.out.println(pageInfo);
 //		System.out.println("pageInfo: "+pageInfo.getNavigatepageNums());
 		return "list";
 	}
-
 
 }
